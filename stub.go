1e595f787cd4ff077f197db742f5a17c83192bea
xaos <xaos@xaos.tech>
2023-02-25 10:30:08 +0100

    feat: define caddy module

--- stub.go
+++ stub.go
@@ -1,1 +1,24 @@
 package stub
+
+import (
+	"github.com/caddyserver/caddy/v2"
+)
+
+type StubDNS struct {
+	// the address & port on which to serve DNS for the challenge
+	Address string `json:"address,omitempty"`
+
+}
+
+
+func init() {
+	caddy.RegisterModule(StubDNS{})
+}
+
+// CaddyModule returns the Caddy module information.
+func (StubDNS) CaddyModule() caddy.ModuleInfo {
+	return caddy.ModuleInfo{
+		ID:  "dns.providers.stub_dns",
+		New: func() caddy.Module {return &StubDNS{}},
+	}
+}
